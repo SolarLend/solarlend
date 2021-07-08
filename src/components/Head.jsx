666d80afff636cd237720b1d0340f999d65494cb
SolarLend <solar-lend@s-tet.com.ua>
2021-07-08 22:14:51 +0300

    Add image

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -17,7 +17,7 @@ const Head = (props) => {
             <title> {lang === "uk" ? "Сонячні електростанції в Україні | Solarland" : "Солнечные электростанции в Украине | Solarland"} </title>
             <link rel="icon" href={favicon} />
             <meta property="og:title" content={lang === "uk" ? "Сонячні електростанції в Україні" : "Солнечные электростанции в Украине"} />
-            <meta property="og:description" name="description" content="Солнечные электростанции в Украине | SolarLend" />
+            <meta property="og:description" name="description" content="Солнечные электростанции в Украине | Solarland" />
             <meta property="og:image" content="https://admin.solarland.com.ua/wp-content/uploads/2021/07/666.jpg" />
             <meta property="og:image:secure_url" content="https://admin.solarland.com.ua/wp-content/uploads/2021/07/666.jpg" />
             <meta property="og:image:url" content="https://admin.solarland.com.ua/wp-content/uploads/2021/07/666.jpg" />
