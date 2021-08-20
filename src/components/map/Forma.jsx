04cadbde56ac9caefd3a4a8f27b2483031ed9d67
Kirill <48245126+shalaevk@users.noreply.github.com>
2021-08-20 19:10:15 +0300

    Added target

--- src/components/map/Forma.jsx
+++ src/components/map/Forma.jsx
@@ -199,15 +199,10 @@ const ContactForm = (props) => {
 
                                 </fieldset>
                                 <div>
-                                    <button className={classNames('pop', cl.formBtn, `${isSubmitting ? 'disabled' : ''}`)}
+                                    <button dangerouslySetInnerHTML={{ __html: onsubmit="gtag('event', 'заявка', { 'event_category': 'zayavka', 'event_action':'send_zayavka'});"}} className={classNames('pop', cl.formBtn, `${isSubmitting ? 'disabled' : ''}`)}
                                         type="submit"
                                         value="Send Message"
                                         disabled={isSubmitting}
-                                      onSubmit = { function( event ) {
-	gtag("event", 'заявка', { "event_category": 'zayavka'});
-   }
-}
-
                                     >
                                         {props.lang === "uk" ? "Замовити" : "Заказать"}
 
