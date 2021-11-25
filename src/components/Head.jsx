f89729c35705c2110220d686049a4a504270e0b9
SolarLend <87077601+SolarLend@users.noreply.github.com>
2021-11-25 14:36:08 +0200

    Added pixel code

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -21,6 +21,12 @@ const Head = (props) => {
             <meta property="og:image" content="https://admin.solarland.com.ua/wp-content/uploads/2021/07/666.jpg" />
             <meta property="og:image:secure_url" content="https://admin.solarland.com.ua/wp-content/uploads/2021/07/666.jpg" />
             <meta property="og:image:url" content="https://admin.solarland.com.ua/wp-content/uploads/2021/07/666.jpg" />
+            
+            <!-- Facebook Pixel Code --><script> !function(f,b,e,v,n,t,s) {if(f.fbq)return;n=f.fbq=function(){n.callMethod? n.callMethod.apply(n,arguments):n.queue.push(arguments)}; if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0'; n.queue=[];t=b.createElement(e);t.async=!0; t.src=v;s=b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t,s)}(window, document,'script', 'https://connect.facebook.net/en_US/fbevents.js'); fbq('init', '452446356310331'); fbq('track', 'PageView'); </script>
+<noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=452446356310331&ev=PageView&noscript=1" /></noscript>
+
+<!-- End Facebook Pixel Code -->
+<meta name="facebook-domain-verification" content="0e9kqir83xd56oflx6u7pu24opkkgu" />
 
         </Helmet>
     )
