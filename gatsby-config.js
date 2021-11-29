6cbd64ac4c565df2e3c21bcd44f7f68dfe34c13c
Yurii Diachenko <yuriidiachenko@Mac-mini-Yurii.local>
2021-11-29 10:27:12 +0200

    Added tag manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,6 +38,25 @@ module.exports = {
         pixelId: "452446356310331",
       },
     },
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-KWZFDB6",
+  
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+  
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+  
+   
+      },
+    },
 
   ],
 };
